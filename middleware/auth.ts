248e6d70ef92fdec7cd5164625bba680dfa3acb9
应凯利 <ying_kaili@finsung.com>
2023-09-25 10:21:03 +0800

    chroe:初始化项目

---
+++ middleware/auth.ts
@@ -0,0 +1,4 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  // isAuthenticated() is an example method verifying if a user is authenticated
+  // return navigateTo("/about");
+});
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <Alert> This is an auto-imported component </Alert>
+  </div>
+</template>
